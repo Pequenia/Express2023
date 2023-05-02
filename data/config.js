10dd3244448361f2319b6e2b086e3a669ef7a0d5
Pequenia <nayelozu.xv@gmail.com>
2023-05-02 09:41:38 -0700

    coneccion mysql

--- data/config.js
+++ data/config.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const config = {
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '1234',
     database: 'api',
 };
 
---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const { response } = require('express');
+const pool = require('../data/config');
+const router = app =>{
+    app.get('/',(request, response) =>{
+        response.send({
+            message: 'Bienvenido a Node.js Express REST API!'
+        });
+    });
+}
+app.get('/users', (request, response)=>{
+    pool.query('SELECT * FROM users', (error, result)=>{
+        if(error) throw error;
+        response.send(result);
+    });;
+});
