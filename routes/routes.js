3c0ed84eacca7955691b636501a408cc7c3513b4
Pequenia <nayelozu.xv@gmail.com>
2023-05-08 09:30:20 -0700

    Actualizar un usuario existente

--- routes/routes.js
+++ routes/routes.js
@@ -36,6 +36,15 @@ const router = app => {
         response.status(201).send (`User added with ID: ${result.insertID}`);
     });
   });
+  //Actualizar un usuario existente
+  app.put('/users/:id',(request, response)=>{
+    const id=request.params.id;
+
+    pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error,result)=>{
+        if (error) throw error;
+        response.send('User updated seccessfully.');
+    });
+  });
 
 }
 
