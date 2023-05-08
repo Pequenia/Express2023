6753f4f5f55be3c66db1a58ddeb631d6f630c641
Pequenia <nayelozu.xv@gmail.com>
2023-05-08 09:18:03 -0700

    Mostrar un solo usuairo ID

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 //Ruta de la app
+const { request } = require('http');
 const pool = require('../data/config');
+const { response } = require('express');
 const router = app => {
   //Mostrar mensaje de bienvenida de root
   app.get('/', (request, response) => {
@@ -16,6 +18,17 @@ const router = app => {
         
     });
   });
+
+  // Mostrar un solo usuairo ID
+  app.get('/users/:id', (request,response)=>{
+    const id= request.params.id;
+
+    pool.query('SELECT * FROM users WHERE id = ?', id,(error,result)=>{
+        if (error) throw error;
+        response.send(result);
+    });
+  });
+
 }
 
-module.exports = router;
+
