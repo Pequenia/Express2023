a94d52d8a6ef5fe1b02e2818048508213aeae1a2
Pequenia <nayelozu.xv@gmail.com>
2023-05-08 09:49:29 -0700

    TABLA PRODUCTOS get post delete update

--- routes/routes.js
+++ routes/routes.js
@@ -54,6 +54,54 @@ const router = app => {
         response.send('User deleted.');
     });
   });
+
+//TABLA PRODUCTOS
+
+  //Mostrar todos los usuarios
+  app.get('/productos', (request, response) => {
+    pool.query('SELECT * FROM users', (error, result) => {
+      if (error) throw error;
+      response.send(result);
+        
+    });
+  });
+
+  // Mostrar un solo usuairo ID
+  app.get('/productos/:id', (request,response)=>{
+    const id= request.params.id;
+
+    pool.query('SELECT * FROM users WHERE id = ?', id,(error,result)=>{
+        if (error) throw error;
+        response.send(result);
+    });
+  });
+  //Agregar un nuevo usuario
+  app.post('/productos', (request, response)=>{
+    pool.query('INSERT INTO users SET ?', request.body,(error, result)=>{
+        if (error) throw error;
+
+        response.status(201).send (`User added with ID: ${result.insertID}`);
+    });
+  });
+  //Actualizar un usuario existente
+  app.put('/productos/:id',(request, response)=>{
+    const id=request.params.id;
+
+    pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error,result)=>{
+        if (error) throw error;
+        response.send('User updated seccessfully.');
+    });
+  });
+  //Eliminar usuario
+  app.delete('/productos/:id',(request, response)=>{
+    const id = request.params.id;
+
+    pool.query('DELETE FROM users WHERE id = ?',id, (error,result)=>{
+        if (error) throw error;
+        response.send('User deleted.');
+    });
+  });
+
 }
 
 module.exports = router;
