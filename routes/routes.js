160bd8100738c8b31828e34f55bcee91b5f000ad
Pequenia <nayelozu.xv@gmail.com>
2023-05-08 09:24:40 -0700

    Agregar un nuevo usuario

--- routes/routes.js
+++ routes/routes.js
@@ -28,7 +28,15 @@ const router = app => {
         response.send(result);
     });
   });
+  //Agregar un nuevo usuario
+  app.post('/users', (request, response)=>{
+    pool.query('INSERT INTO users SET ?', request.body,(error, result)=>{
+        if (error) throw error;
+
+        response.status(201).send (`User added with ID: ${result.insertID}`);
+    });
+  });
 
 }
 
-
+module.exports = router;
