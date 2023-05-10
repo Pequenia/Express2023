eaea307b95cf6bfea8f6d2619126ddb6b2668b34
Pequenia <nayelozu.xv@gmail.com>
2023-05-09 21:42:34 -0700

    agrgue nombre

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const router = app => {
   //Mostrar mensaje de bienvenida de root
   app.get('/', (request, response) => {
     response.send({
-      message: "Bienvenido a Node.js Express REST API!",
+      message: "Bienvenido a Node.js Express REST API! de NAYELI LOPEZ ZUÑIGA",
     });
   });
 
